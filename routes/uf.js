d16e2e2d9dda35f1ed63f68ffb2ba34aca9a8ff6
ivanvalmorbida <ivanvalmorbida@gmail.com>
2017-03-26 23:36:48 -0300

    Ivan

---
+++ routes/uf.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router  = express.Router();
+var settings = require("../settings");
+var mysql   = require('mysql');
+
+exports.AllRecords = function (req, res) {
+    var connection = mysql.createConnection(settings.dbConect);
+
+    connection.connect();
+
+    connection.query('SELECT * from tbuf', function(err, rows, fields) {
+        if (!err) {
+            res.json({imovel_favo: rows});
+            //console.log('The solution is: ', rows);
+        }
+        else
+            console.log('Error while performing Query.');
+    });
+}
+
