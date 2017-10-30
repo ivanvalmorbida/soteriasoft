1628e1990fca4316e0ea87fc8a65ee28b7485f33
ivanvalmorbida <ivanvalmorbida@gmail.com>
2017-10-30 17:16:47 -0200

    636

--- public/js/imovel.js
+++ public/js/imovel.js
@@ -1,4 +1,4 @@
-angular.module('MyApp', ['ngMaterial','ui.mask'])
+angular.module('MyApp', ['ngMaterial', 'ui.mask', 'MyApp.Comum'])
 .controller('AppCtrl', function($http, $scope, $mdDialog) {
     $scope.format = function(mask, number) {
         return format(mask, number);
@@ -337,58 +337,5 @@ angular.module('MyApp', ['ngMaterial','ui.mask'])
     }
     else{
         $scope.Limpar();         
-    }
-}).config(function($mdDateLocaleProvider) {
-    $mdDateLocaleProvider.formatDate = function(date) {
-        return date ? moment(date).format('DD/MM/YYYY') : '';
-    };
-
-    $mdDateLocaleProvider.parseDate = function(dateString) {
-        var m = moment(dateString, 'DD/MM/YYYY', true);
-        return m.isValid() ? m.toDate() : new Date(NaN);
-    };
-    }).directive('uiTelefone', function(){
-    return {
-        require: 'ngModel',
-        link: function(scope, element, attr, ctrl){
-        var _formatTelefone = function(telefone){
-            //(99)9999-9999 - 13dig
-            //(99)99999-9999 - 14dig
-            telefone = telefone.replace(/[^0-9]+/g, "");                
-            if(telefone.length > 0){
-                telefone = telefone.substring(-1,0) + "(" + telefone.substring(0);
-            }
-            if(telefone.length > 3){
-                telefone = telefone.substring(0,3) + ")" + telefone.substring(3);
-            }
-            if(telefone.length == 12){
-                telefone = telefone.substring(0,8) + "-" + telefone.substring(8);
-            }else if(telefone.length >= 13){
-                telefone = telefone.substring(0,9) + "-" + telefone.substring(9,13);
-            }
-            return telefone;
-        }
-        element.bind('keyup', function(){
-            ctrl.$setViewValue(_formatTelefone(ctrl.$viewValue));
-            ctrl.$render();
-        });
-        }
-    }
-}).directive('uiCep', function(){
-    return {
-        require: 'ngModel',
-        link: function(scope, element, attr, ctrl){
-        var _formatCep = function(cep){
-            cep = cep.replace(/[^0-9]+/g, "");                
-            if(cep.length > 5){
-                cep = cep.substring(0,5) + "-" + cep.substring(5,8);
-            }
-            return cep;
-        }
-        element.bind('keyup', function(){
-            ctrl.$setViewValue(_formatCep(ctrl.$viewValue));
-            ctrl.$render();
-        });
-        }
     }
 });
