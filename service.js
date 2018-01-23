92410f8eca4cc2f944175669d198a810fff59439
ivanvalmorbida <ivanvalmorbida@gmail.com>
2018-01-23 10:21:16 -0200

    ivan

--- service.js
+++ service.js
@@ -1,4 +1,26 @@
-var Service = require('node-service-linux').Service;
+/*
+npm install -g node-windows
+npm link node-windows
+*/
+
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'SoteriaSoft',
+  description: 'SoteriaSoft',
+  script: 'C:\\devel\\soteriasoft\\app.js'
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+  svc.start();
+});
+
+svc.install();
+
+/*var Service = require('node-service-linux').Service;
 
  var svc = new Service({
    name:'SoteriaSoft',
@@ -10,4 +32,4 @@ var Service = require('node-service-linux').Service;
    svc.start();
  });
 
- svc.install();
+ svc.install();*/
